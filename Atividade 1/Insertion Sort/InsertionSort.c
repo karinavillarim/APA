627999b42dea6bd2af21180cabd726efff37e4c2
karinavillarim <karinavillarim@hotmail.com>
2018-07-26 15:16:52 -0300

    Update InsertionSort.c

--- Atividade 1/Insertion Sort/InsertionSort.c
+++ Atividade 1/Insertion Sort/InsertionSort.c
@@ -24,10 +24,12 @@ void InsertionSort(int *array){
 
 int main(void){
     
-    int array[MAX_ELEMENTOS] = {4,2,9,5,1,7,8,3,0,6};
+    int array1[MAX_ELEMENTOS] = {4,2,9,5,1,7,8,3,0,6}; //caso aleatório
+    int array2[MAX_ELEMENTOS] = {0,1,2,3,4,5,6,7,8,9}; //melhor caso (ordenado)
+    int array3[MAX_ELEMENTOS] = {9,8,7,6,5,4,3,2,1,0}; //pior caso (ordenado ao contrário)
     int i = 0;
     
-    InsertionSort(array);
+    InsertionSort(array1);
     
     printf("{");
     for(i = 0; i <= MAX_ELEMENTOS - 1; i++){

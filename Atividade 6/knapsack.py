1780d0b2fe4b0c71da004c28f203c4be0241ee0e
karinavillarim <karinavillarim@hotmail.com>
2018-10-18 13:04:38 -0300

    Update knapsack.py

--- Atividade 6/knapsack.py
+++ Atividade 6/knapsack.py
@@ -1,52 +1,43 @@
-entrada = 'mochila1000.txt'
+from sys import argv
+from itertools import islice
 
-def load(entrada):
+def load(arq='entrada.txt'):
+    n, m, obj = [],[],[] #Inicializa n, m e a lista de objetos
+    with open(arq,'r') as entrada:
+        n, m = (entrada.readline()).rstrip().split(" ") #Extrai o n e m da primeira linha 
+        obt = (list((islice(entrada,0,None)))) #Salva o resto do arquivo em uma lista temporária
+        for x in obt:
+            unidade = list(map(int, x.rstrip().split(" "))) #Converte em inteiros, salva na lista obj
+            obj.append(unidade)
+    m, n = int(m), int(n)
     
-    valores = []
-    pesos = []
-    obj = []
-    
-    with open(entrada,'r') as entrada: 
-            for line in entrada: 
-                linha = list(map(int, line.rstrip().split(" ")))
-                obj.append(linha)
+    return n,m,obj
 
-    n = obj[0][0]
-    m = obj[0][1] #Guarda a capacidade da mochila
+def knapsack(n,m,obj):
 
-    for i in range(1,(len(obj))):
-        pesos.append(obj[i][0]) #Ordena os pesos num vetor
-    
-    for i in range(1,(len(obj))):
-        valores.append(obj[i][1]) #Ordena os valores num vetor
+    tabela = [[0 for q in range(m+1)] for t in range(n+1)] #Cria uma tabela com linhas para cada item, e colunas para cada peso.
+    for i in range(1,n+1): #Loop de colunas
+        for j in range(1,m+1): #Loop das linhas
+            if obj[i-1][0] <= j: 
+                tabela[i][j] = max(tabela[i-1][j], obj[i-1][1] + tabela[i-1][j - obj[i-1][0]]) #Procura o valor máximo
+            else: tabela[i][j] = tabela[i-1][j] #Caso contrário, retorna o valor da linha na coluna anterior
 
-    return n,m,pesos,valores
+    #Impressao de quais itens foram selecionados
+    t_n, t_m, t_it = n, m, []
 
-def knapSack(n,m,pesos,valores):
-    K = [[0 for x in range(m+1)] for x in range(n+1)]
- 
-    for i in range(n+1):
-        for w in range(m+1):
-            if i==0 or w==0:
-                K[i][w] = 0
-            elif pesos[i-1] <= w:
-                K[i][w] = max(valores[i-1] + K[i-1][w-pesos[i-1]],  K[i-1][w])
-            else:
-                K[i][w] = K[i-1][w]
-
-    a = n
-    b = m
-    itens = []
-
-    while a and b > 0:
-        if K[a][b] != K[a-1][b]:
-            itens.append(a)
-            b = b - pesos[a-1]
-            a = a - 1
+    while t_n and t_m > 0:
+        if tabela[t_n][t_m] != tabela[t_n-1][t_m]: #Se o valor da linha na coluna for diferente da coluna acima imediatamente desta, marca o valor 
+            t_it.append(t_n)
+            t_n -= 1
+            t_m -= obj[t_n][0]
         else:
-            a = a - 1
+                t_n -= 1
 
-    print("Total de " + str(len(itens)) + " itens, sendo eles: " + str(itens) + "\nCom valor total: " + str(K[n][m]))
+    print("Solução: \n Um total de " + str(len(t_it)) + " itens: " ,end='')
+    for x in t_it: print(x, end=', ')
+    print("com o valor total de " + str(tabela[n][m]))
 
-n,m,pesos,valores = load(entrada)
-knapSack(n,m,pesos,valores)
+
+n,m,obj = load("mochila1000.txt")
+
+knapsack(n,m,obj)
